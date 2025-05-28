71302b229d46d806f69f9b73ec239730792396ee
ganavi-20 <ganavireddy0803@gmail.com>
2025-05-28 10:55:06 +0530

    i have added few lines of code

--- server.js
+++ server.js
@@ -2,3 +2,5 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const passwordRoutes = require('./routes/passwordRoutes');
+const { errorHandler, notFoundHandler } = require('./middleware/errorMiddleware');
