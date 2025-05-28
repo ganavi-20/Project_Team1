7e07748b13d136c000eda2b3e007c00b593e79a7
ganavi-20 <ganavireddy0803@gmail.com>
2025-05-28 11:18:21 +0530

    i have added few lines of code

---
+++ password.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const {
+  generateSecurePassword,
+  generateBatchPasswords,
+  generatePassphrase,
+  analyzePassword,
+  calculateEntropy,
+  getCharacterSets
+} = require('../services/passwordService');
+
